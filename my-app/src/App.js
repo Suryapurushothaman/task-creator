b00cb4b9be9594ad2a191b43177313e09a28e1fa
surya <suryaayrus44@gmail.com>
2021-05-01 16:15:42 +0530

    feat: add basic react architecture

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import DatePicker from './elements/DatePicker'
+function App() {
+  return (
+    <div className="App">
+      <DatePicker />
+    </div>
+  );
+}
+
+export default App;
